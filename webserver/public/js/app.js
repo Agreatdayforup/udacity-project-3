2763762c4f9550bad3ce51ee09d2b7c1c1f937f8
Patrick <cayerp@gmail.com>
2019-12-06 11:02:36 -0500

    cleaned up code
    cleaned up code and got some items to print to webpage

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -1,15 +1,20 @@
 //mport { request } from "http";
 console.log('Client side js is loaded')
-//const request = require('request')
-//const https = require('https')
+
+
 
 /* Global Variables */
 
-// function to take user zipcode
-// function to take user feelings
+// const to take user zipcode and user feelings
 const searchForm = document.querySelector('form')
 const searchZip = document.querySelector('input')
 const feelingSearch = document.querySelector('textarea')
+
+const userTemp = document.querySelector('#temp')
+const userDate = document.querySelector('#date')
+const userContent = document.querySelector('#content')
+
+
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -17,28 +22,21 @@ searchForm.addEventListener('submit', (e) => {
     const zipcode = searchZip.value
     const fs = feelingSearch.value
 
-
     fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&APPID=31ccdce38d7be8cba4e13d567e1d43db&units=imperial').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
+        response.json().then((data) => {
+            console.log(data) 
+            if (data.error) {
+                
+            } else {
+            userTemp.textContent = 'The temperature for ' + zipcode + ' is ' + data.forecast + '.'
+            userContent.textContent = fs
+            }
     })
 })
 
     console.log(zipcode)
     console.log(fs)
 })
-
-
-
-
-// below used to concat into a url with api
-// const location = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&APPID=31ccdce38d7be8cba4e13d567e1d43db'
-// const weatherURL = location
-
-// // requesting weather from open weather
-// request({url: weatherURL, json: true}, (error, response) => {
-//     console.log(response.body.main.temp)
-// })
 
 
 // Create a new date instance dynamically with JS
--- webserver/src/server.js
+++ webserver/src/server.js
@@ -1,39 +1,16 @@
 const path = require('path')
 const bodyParser = require('body-parser')
+
 // Require Express to run server and routes
 const express = require('express')
 const app = express()
-//const forecast = require('./utils/forecast')
+
+const forecast = require('./utils/forecast')
+
 
 // Setup empty JS object to act as endpoint for all routes
 //projectData = {};
 
-// app.get('', (req, res) => {
-//     if (!req.query.address) {
-//         return res.send({
-//             error: 'You must provide a zipcode!'
-//         })
-//     }
-
-//     forecast(req.query.address, (error, {zipcode, location}) => {
-//         if (error) {
-//             return res.send({ error })
-//         }
-
-//         forecast(zipcode, (error, forecastData) => {
-//             if (error) {
-//                 return res.send({ error })
-//             }
-
-//             res.send({
-//                 forecast: forecastData,
-//                 zipcode
-//             })
-//         })
-//     })
-
-    
-// })
 
 
 // Start up an instance of app
@@ -47,7 +24,7 @@ app.use(bodyParser.json());
 const cors = require('cors')
 
 
-// Initialize the main project folder
+// Initialize the main project folder setsup static directory 
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 console.log(path.join(__dirname, '../public'))
