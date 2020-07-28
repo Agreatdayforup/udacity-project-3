73fbf917b2e8f5eec59d773ff1b6d82fa39814e5
Patrick C <35810793+Agreatdayforup@users.noreply.github.com>
2020-07-28 15:57:05 -0400

    Update app.js

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -14,8 +14,9 @@ const searchForm = document.querySelector('form')
 const searchZip = document.querySelector('input')
 const feelingSearch = document.querySelector('textarea')
 
-
-
+// API return data
+const apiWeather = {}
+    apiWeather.temp
 
 // listens to user inputs and submittion of the form 
 searchForm.addEventListener('submit', (e) => {
@@ -25,17 +26,18 @@ searchForm.addEventListener('submit', (e) => {
     const fs = feelingSearch.value
 
     // grabs temperature data from weather api
-    fetch('https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&APPID=31ccdce38d7be8cba4e13d567e1d43db&units=imperial')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&APPID=31ccdce38d7be8cba4e13d567e1d43db&units=imperial`)
         .then(response => { 
         let data = response.json();
         return data;
     })
-        .then(data => {
-        userTemp.textContent = 'It is currently' + data['main']['temp'] + ' degrees F outside'
-        
-        console.log(data.main.temp)
-
+    .then(data => {
+        apiWeather.temp.value = data.main.temp
     })
+    .then( => {
+        userTemp.textContent = `It is currently ${apiWeather.temp.value} degrees F outside`
+        console.log(apiWeather.temp.value)
+     }
         userContent.textContent = fs
     
     console.log(zipcode)
